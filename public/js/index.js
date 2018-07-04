e6d5750ed8567c07dfe58b12a23df9ac9e0cd0fe
FFVictory <vasiljev.andrew@gmail.com>
2018-07-04 05:40:52 +0300

    initial css

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+document.getElementById("valBox").innerHTML=500;
+
+function showVal(newVal , weaponName){
+    document.getElementById("valBox").innerHTML=newVal;
+
+    var audio = document.getElementById('audio');
+    var source = document.getElementById('audioSource');
+    source.src = 'audio/'+weaponName+'-'+newVal+'.mp3';
+
+    audio.load(); //call this to just preload the audio without playing
+    /*audio.play(); //call this to play the song right away*/
+}

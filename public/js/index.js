5dbd921bd17260442a2526acaa1a86d08a5d66a6
FFVictory <vasiljev.andrew@gmail.com>
2018-07-04 21:01:38 +0300

    elements ids sorted

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,10 @@
 document.getElementById("valBox").innerHTML=500;
 
-function showVal(newVal , weaponName){
-    document.getElementById("valBox").innerHTML=newVal+'m';
+function showVal(newVal , weaponName,silenced){
+    document.getElementById("valBox"+weaponName+silenced).innerHTML=newVal+'m';
 
-    var audio = document.getElementById('audio');
-    var source = document.getElementById('audioSource');
+    var audio = document.getElementById('audio'+weaponName+silenced);
+    var source = document.getElementById('audioSource'+weaponName+silenced);
     source.src = 'audio/'+weaponName+'-'+newVal+'.mp3';
 
     audio.load(); //call this to just preload the audio without playing

9415a6d3f12ed35b658569a263a4928989b470d2
FFVictory <vasiljev.andrew@gmail.com>
2018-07-05 05:10:59 +0300

    removed most of the stubs and added support for firemode

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
-function showVal(newVal , weaponName,silenced){
+function showVal(newVal , weaponName,silenced,firemode){
     document.getElementById("valBox"+weaponName+silenced).innerHTML='Current range is : '+newVal+'m';
-    var audio = document.getElementById('audio'+weaponName+silenced);
-    var source = document.getElementById('audioSource'+weaponName+silenced);
-    source.src = 'audio/'+weaponName+'-'+newVal+'-'+silenced+'.mp3';
+    var audio = document.getElementById('audio'+weaponName+silenced+firemode);
+    var source = document.getElementById('audioSource'+weaponName+silenced+firemode);
+    source.src = 'audio/'+weaponName+'-'+newVal+'-'+silenced+'-'+firemode+'.mp3';
 
     audio.load(); //call this to just preload the audio without playing
     /*audio.play(); //call this to play the song right away*/

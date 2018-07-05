cc692440dc81c49a94b4947789c684fca9e4a1ea
FFVictory <vasiljev.andrew@gmail.com>
2018-07-05 13:27:56 +0300

    Radio button to switch between silenced and unsilenced mode

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,15 @@
 angular.module('app', [])
     .controller('maincontroller' , function(){
         var vm = this;
-        vm.test = "TESTVALUE";
+        vm.content = "true";
+        vm.isSilenced = function(){
+            if(vm.content == "true") {
+                console.log("true");
+                return true;
+            }
+            else{
+                console.log("false");
+                return false;
+            }
+        }
     })

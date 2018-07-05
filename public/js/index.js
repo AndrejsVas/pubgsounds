32324ad27c8654dd9ebc2db17848b9acbe4ab3c1
FFVictory <vasiljev.andrew@gmail.com>
2018-07-05 03:58:43 +0300

    changed audio naming format

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ function showVal(newVal , weaponName,silenced){
     document.getElementById("valBox"+weaponName+silenced).innerHTML='Current range is : '+newVal+'m';
     var audio = document.getElementById('audio'+weaponName+silenced);
     var source = document.getElementById('audioSource'+weaponName+silenced);
-    source.src = 'audio/'+weaponName+'-'+newVal+'.mp3';
+    source.src = 'audio/'+weaponName+'-'+newVal+'-'+silenced+'.mp3';
 
     audio.load(); //call this to just preload the audio without playing
     /*audio.play(); //call this to play the song right away*/

8184663381f98e9feab74318450c1ce87d54b4ea
FFVictory <vasiljev.andrew@gmail.com>
2018-07-06 21:59:23 +0300

    added max and min ranges properties to weapons

--- public/js/app.js
+++ public/js/app.js
@@ -8,14 +8,19 @@ angular.module('app', [])
 
 
 		vm.init = function() {
-			var weapons = ["P1911", "R45", "R1895", "P92", "P18C", "SAWED_OFF", "S12K", "S1897", "S686", "MICRO_UZI", "KRISS_VECTOR", "UMP9", "TOMMY_GUN", "M16A4", "M416", "QBZ95", "SCAR_L", "AKM", "VSS", "MINI14", "SKS", "SLR", "WIN94", "KARABINER_98_KURZ", "M24", "DP_28"];																				
-			var i;
+			var weapons = ["P1911", "R45"] /*"R1895", "P92", "P18C", "SAWED_OFF", "S12K", "S1897", "S686", "MICRO_UZI", "KRISS_VECTOR", "UMP9", "TOMMY_GUN", "M16A4", "M416", "QBZ95", "SCAR_L", "AKM", "VSS", "MINI14", "SKS", "SLR", "WIN94", "KARABINER_98_KURZ", "M24", "DP_28"];																				*/
+            var weaponRangeUnsilenced = ["300","100"];
+            var weaponRangeSilenced = ["100" , "100"];
+            var i;
 			for (i = 0; i < weapons.length; i++) {
                 eval(weapons[i] + " = new Object()");
                 eval("vm.weapons." + weapons[i] + " = eval(weapons[i])");
                 eval(weapons[i]).firemode = "single";
                 eval(weapons[i]).rangeBar = 0;
-                /*eval(weapons[i]). = 0;*/
+                eval(weapons[i]).minRange = 0;
+                eval(weapons[i]).maxRangeUnsilenced = weaponRangeUnsilenced[i];
+                eval(weapons[i]).maxRangeSilenced = weaponRangeSilenced[i];
+
 
                 eval(weapons[i]).issilenced = '0';
                 vm.weapons[i] = eval(weapons[i]);

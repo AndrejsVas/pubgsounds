6fc236fb2575c618a42e89958e7752b54879a905
Artur <123@inbox.lv>
2018-07-07 23:08:28 +0300

    added legal terms + burst radio button

--- public/js/app.js
+++ public/js/app.js
@@ -7,23 +7,23 @@ angular.module('app', [])
 
 
         vm.init = function () {
-            var weapons = ["M416", "R45"]
+            var weapons = ["M416", "R45", "M16A4"]
             /*"R1895", "P92", "P18C", "SAWED_OFF", "S12K", "S1897", "S686", "MICRO_UZI", "KRISS_VECTOR", "UMP9", "TOMMY_GUN", "M16A4", "M416", "QBZ95", "SCAR_L", "AKM", "VSS", "MINI14", "SKS", "SLR", "WIN94", "KARABINER_98_KURZ", "M24", "DP_28"];																				*/
-            var weaponRangeUnsilenced = ["600", "300"];
-            var weaponRangeSilenced = ["300", "0"];
-            var weaponCategory = ["Assault rifles", "Pistols"];
+            var weaponRangeUnsilenced = ["600", "300", "400"];
+            var weaponRangeSilenced = ["300", "0", "200"];
+            var weaponCategory = ["Assault rifles", "Pistols", "Assault rifles"];
 /*          Variable to help html determine what kind of silencer the weapon has
             1- only unsilenced
             2- silenced + unsilenced
             3 - only silenced*/
-            var weaponSilencedType = [2 , 1];
+            var weaponSilencedType = [2 , 1,  2];
 /*          Variable to determine what kind of firemodes the weapon has
             1 - only single
             2 - single + auto
             3- only auto
             4 - single + burst  +auto
             5 - single + burst*/
-            var weaponFireModes = [2,1];
+            var weaponFireModes = [2,1,5];
             var i;
             for (i = 0; i < weapons.length; i++) {
 				vm.weapons[i] = new Object();

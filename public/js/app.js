758984079ab1ea24c13fd0b99061f9a734643078
Artur <123@inbox.lv>
2018-07-07 22:43:12 +0300

    working weapon categories

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,11 @@ angular.module('app', [])
 
 
         vm.init = function () {
-            var weapons = ["M416", "M416"]
+            var weapons = ["M416", "R45"]
             /*"R1895", "P92", "P18C", "SAWED_OFF", "S12K", "S1897", "S686", "MICRO_UZI", "KRISS_VECTOR", "UMP9", "TOMMY_GUN", "M16A4", "M416", "QBZ95", "SCAR_L", "AKM", "VSS", "MINI14", "SKS", "SLR", "WIN94", "KARABINER_98_KURZ", "M24", "DP_28"];																				*/
-            var weaponRangeUnsilenced = ["600", "600"];
-            var weaponRangeSilenced = ["300", "300"];
-            var weaponCategory = ["ar", "pistol"];
+            var weaponRangeUnsilenced = ["600", "300"];
+            var weaponRangeSilenced = ["300", "0"];
+            var weaponCategory = ["Assault rifles", "Pistols"];
 /*          Variable to help html determine what kind of silencer the weapon has
             1- only unsilenced
             2- silenced + unsilenced
